75bdfc3187ad2f346bcd2752db39ddcc4f0eef1b
Roman Gardukevich <roman@gardukevich.com>
2020-02-29 20:31:04 +0300

    Initial Commit

---
+++ ProjectWithTeam/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProjectWithTeam
+//
+//  Created by Roman Gardukevich on 29.02.2020.
+//  Copyright © 2020 Roman Gardukevich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
